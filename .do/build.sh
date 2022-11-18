52047bbf8e5514da93a0901e8326a5b2cc1fb00c
BarnabyRumbold <barnabyrumbold@hotmail.com>
2022-11-18 11:25:12 +0100

    commit

--- .do/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-virtualenv virtualenv
-source virtualenv/bin/activate
-pip install -r requirements.txt
---
+++ packages/medito/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+virtualenv virtualenv
+source virtualenv/bin/activate
+pip install -r requirements.txt --target virtualenv/lib/python3.9/site-packages
